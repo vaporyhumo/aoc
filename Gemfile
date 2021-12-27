420f05526805cdc85c4d4813361b80773cb97c43
vaporyhumo <roanvilina@gmail.com>
2021-12-26 23:25:35 -0300

    Add `day4`

--- Gemfile
+++ Gemfile
@@ -9,3 +9,7 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 gem 'rubocop', '~> 1.23'
 
 gem 'rspec', '~> 3.10'
+
+gem "awesome_print", "~> 1.9"
+
+gem "matrix", "~> 0.4.2"
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -5,6 +5,8 @@ require_relative '../lib/day1'
 describe Day1 do
   specify do
     expect(described_class.problem_a(described_class::EXAMPLE)).to be(7)
+    expect(described_class.problem_a(described_class::INPUT)).to be(1393)
     expect(described_class.problem_b(described_class::EXAMPLE)).to be(5)
+    expect(described_class.problem_b(described_class::INPUT)).to be(1359)
   end
 end
