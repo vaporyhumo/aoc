3ae8c921dba40f6518b396c7306e48d0327157fe
vaporyhumo <roanvilina@gmail.com>
2022-02-23 17:23:28 -0300

    Add day 1 for haskell

--- haskell/1a.hs
+++ haskell/1a.hs
@@ -1,11 +1,21 @@
+input :: [Integer]
 input = [199,200,208,210,200,207,240,269,260,263]
-map_lines func string = map func (lines string)
-each_cons = zip<*>tail
-count_filter pred = length . filter pred
-output numbers = count_filter id [j < k | (j, k) <- each_cons numbers]
 
+mapLines :: (String -> b) -> String -> [b]
+mapLines func string = map func (lines string)
+
+eachCons :: [a] -> [(a, a)]
+eachCons = zip<*>tail
+
+countFilter :: (a -> Bool) -> [a] -> Int
+countFilter pred = length . filter pred
+
+output :: Ord a => [a] -> Int
+output numbers = countFilter id [j < k | (j, k) <- eachCons numbers]
+
+main :: IO ()
 main = do
-  putStrLn (show $ output input)
-  file_input <- readFile "input.txt"
-  let numbers = map_lines read file_input :: [Integer]
-  putStrLn (show $ output numbers)
+  print (output input)
+  file_input <- readFile "../inputs/input1.txt"
+  let numbers = mapLines read file_input :: [Integer]
+  print (output numbers)
